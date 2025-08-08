9ff9e4d9f5c1c325d48391305224c477658472cc
“Sangini <sanginijshetty@gmail.com>
2025-08-08 15:32:01 +0530

    Adding HTML Templates

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def home():
+#     return "Hello, World!"
+
+# if __name__ == "__main__":
+#     app.run(debug=True)  creating a simple hello world 
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, World!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
+#  Adding HTML Templates
